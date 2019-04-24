f6e1756ee7bf8b0c52a8ccffdade42776c30c790
Arjun Vikram <arjvik@gmail.com>
2019-04-23 21:24:12 -0500

    fixed input sanitation bug

--- src/main/java/com/arjvik/robotics/ironreignbot/handlers/blogposts/PostAssignHandler.java
+++ src/main/java/com/arjvik/robotics/ironreignbot/handlers/blogposts/PostAssignHandler.java
@@ -3,7 +3,6 @@ package com.arjvik.robotics.ironreignbot.handlers.blogposts;
 import static com.arjvik.robotics.ironreignbot.stores.BlogPostStore.store;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 
 import com.arjvik.robotics.ironreignbot.BlogPost;
 import com.arjvik.robotics.ironreignbot.handlers.AbstractImperativeHandler;
@@ -17,12 +16,9 @@ public class PostAssignHandler extends AbstractImperativeHandler {
 		if (!content.startsWith("!blog assign"))
 			return;
 		String[] cmd = content.substring("!blog assign".length()).trim().split("\\s+", 2);
-		cmd[0] = formatMention(cmd[0]);
-		if (cmd.length < 2)
+		if (cmd.length < 2 || !isMention(cmd[0]))
 			replyTo(msg, "Invalid usage of `!blog assign <@user> <blog post topic>`");
-		else if (!isMention(cmd[0])) {
-			replyTo(msg, "Invalid user: %s", cmd[0]);
-		} else {
+		else {
 			BlogPost post = new BlogPost(cmd[1], LocalDate.now());
 			store.assignBlogPost(getIDFromMention(cmd[0]), post);
 			replyTo(msg, "Assigned %s blog post \"%s\"", formatMention(cmd[0]), cmd[1]);
