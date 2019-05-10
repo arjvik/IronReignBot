27f59bbfed9e251fb207fba5a52c9c1bbab54223
Arjun Vikram <arjvik@gmail.com>
2019-05-09 21:52:35 -0500

    Update Help command

--- src/main/java/com/arjvik/robotics/ironreignbot/handlers/misc/HelpHandler.java
+++ src/main/java/com/arjvik/robotics/ironreignbot/handlers/misc/HelpHandler.java
@@ -10,13 +10,18 @@ public class HelpHandler extends AbstractMessageHandler {
 
 	@Override
 	protected void onMessageEvent(Message msg, String content) {
-		replyTo(msg, "__**IronReignBot** (created by Arjun Vikram)__\n\n" + "**Commands**\n\n" + "Assign Blog Posts\n"
+		replyTo(msg, "__**IronReignBot** (created by Arjun Vikram)__\n\n"
+				+ "**Commands**\n\n"
+				+ "Assign Blog Posts\n"
 				+ "- `!blog assign <@user> <blog post>`: Assign somebody a blogpost.\n"
 				+ "- `!blog list [<@user> | all]`: List all blog posts assigned to somebody, all users, or by default yourself.\n"
-				+ "- `!blog done [<@user>] <id>`: Mark a post as completed by its id.\n\n" + "Manage Blog Posts\n"
+				+ "- `!blog done [<@user>] <id>`: Mark a post as completed by its id.\n\n"
+				+ "Manage Blog Posts\n"
 				+ "- `!admin clear-posts`: Delete all blog posts.\n"
 				+ "- `!admin export-posts`: Export all posts to JSON.\n"
 				+ "- `!admin import-posts <JSON in triple backticks>`: Import blog posts from JSON.\n\n"
+				+ "The Orange Alliance\n" 
+				+ "- `!toa <team>`: List team information from The Orange Alliance.\n\n"
 				+ "Miscelaneous\n" + "- `!ping`: Respond `Pong!` to test if bot is up.\n"
 				+ "- `!vote`: React with a :thumbsup: and :thumbsdown: to facilitate a vote.\n"
 				+ "- `!help`: Respond with a help message.\n\n" + "Admin Commands\n"
