c9b51e31e024b267540708396640004c001e0568
Arjun Vikram <arjvik@gmail.com>
2021-01-30 19:44:39 -0600

    Split large messages into multiple smaller messages due to Discord's 2k character limitation

--- src/main/java/com/arjvik/robotics/ironreignbot/handlers/AbstractMessageHandler.java
+++ src/main/java/com/arjvik/robotics/ironreignbot/handlers/AbstractMessageHandler.java
@@ -1,23 +1,31 @@
 package com.arjvik.robotics.ironreignbot.handlers;
 
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import reactor.core.publisher.Flux;
 
 public abstract class AbstractMessageHandler implements Handler {
 	
+	public static int MAX_MESSAGE_LENGTH = 1900;
+	
 	protected String prefix;
-
+	
 	protected Logger log = LoggerFactory.getLogger(getClass());
 	
 	@Override
 	public void setCommandPrefix(String prefix) {
 		this.prefix = prefix;
 	}
-
+	
 	@Override
 	public void setupRoute(GatewayDiscordClient client) {
 		client.getEventDispatcher()
@@ -41,8 +49,37 @@ public abstract class AbstractMessageHandler implements Handler {
 	
 	protected abstract void onMessageEvent(Message msg, String content);
 	
-	protected static Message replyTo(Message message, String format, Object... args) {
-		return message.getChannel().flatMap(ch -> ch.createMessage(String.format(format, args))).block();
+	private static Collection<String> splitText(String text) {
+		String[] lines = text.split("(?<=\n)");
+		Deque<String> split = new ArrayDeque<>();
+		split.push("");
+		for (String line : lines) {
+			if ((split.getLast() + line).length() <= MAX_MESSAGE_LENGTH)
+				split.addLast(split.removeLast() + line);
+			else if (line.length() <= MAX_MESSAGE_LENGTH)
+				split.addLast(line);
+			else
+				for (int i = 0; i < line.length(); i += MAX_MESSAGE_LENGTH)
+					split.addLast(String.format("%s%s%s%n",
+							i == 0 ? "" : "...",
+							line.substring(i, Math.min(i + MAX_MESSAGE_LENGTH, line.length())),
+							i + MAX_MESSAGE_LENGTH >= line.length() ? "" : "..."));
+		}
+		return split;
+	}
+	
+	protected List<Message> replyTo(Message message, String format, Object... args) {
+		String text = String.format(format, args);
+		log.info("Response: \"{}\"", text);
+		Collection<String> split = splitText(text);
+		if (split.size() > 1)
+			for (String line : split)
+				log.info("Split message: \"{}\"", line);
+		return message.getChannel()
+					  .flatMapMany(ch -> Flux.fromIterable(split)
+											 .flatMap(ch::createMessage))
+					  .collectList()
+					  .block();
 	}
 	
 	protected static long getIDFromMention(String mention) {
